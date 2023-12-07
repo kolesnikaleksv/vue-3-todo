66e0c6aa6f8d832ede1b052efa73f9676e654fa2
kolesnikaleksv <kolesnikaleksv@gmail.com>
2023-12-07 09:50:35 +0200

    Create counter component

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+</script>
+
+<template>
+  <div class="container pt-5">
+    <div class="card center">
+      <h1>Counter: {{ counter }}</h1>
+      <div>
+        <button class="btn pimary" @click="counter++">+</button>
+        <button class="btn danger" @click="counter--">-</button>
+      </div>
+    </div>
+  </div>
+</template>
