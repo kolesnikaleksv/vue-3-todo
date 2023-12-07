7beee0a72aa9a4b4d85aa5fafb8d525c39f965a3
kolesnikaleksv <kolesnikaleksv@gmail.com>
2023-12-07 11:18:50 +0200

    Add Form component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Card from './components/Card.vue'
+import Form from './components/Form.vue'
 </script>
 
 <template>
   <Card />
+  <Form />
 </template>
---
+++ src/components/Form.vue
@@ -0,0 +1,44 @@
+<script>
+  export default {
+  data() {
+    return {
+      title: 'List of notes',
+      placeholderString: "Write title of note",
+      inputValue: "",
+      notes: ['note 1', 'note 2']
+    }
+  },
+  methods: {
+    inputChangeHanler(e) {
+      this.inputValue = e.target.value
+      console.log(this.inputValue)
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue)
+      this.inputValue = ''
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container pt-5">
+    <div class="card">
+      <h1>{{ title }}</h1>
+      <div class="form-control">
+        <input 
+          type="text" 
+          :placeholder="placeholderString"
+          :value="inputValue"
+          v-on:input="inputChangeHanler">
+      </div>
+      <button class="btn pimary" @click="addNewNote">Add</button>
+      <hr/>
+      <ul class="list">
+        <li class="list-item" v-for="myNote in notes">
+          {{ myNote }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
