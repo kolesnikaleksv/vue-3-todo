dba48dffb36bf2d6508602c2cd55f3673aeb905a
kolesnikaleksv <kolesnikaleksv@gmail.com>
2023-12-12 12:58:00 +0200

    Add new component, work with vue dirrectives

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Card from './components/Card.vue'
 import Form from './components/Form.vue'
+import PartTwo from './components/PartTwo.vue';
 </script>
 
 <template>
+  <PartTwo />
   <Card />
   <Form />
 </template>
---
+++ src/components/PartTwo.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+  data() {
+    return {
+      title: 'Part Two',
+      myHtml: '<h1>My Html</h1>'
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<template>
+  <div class="container pt-5">
+    <div class="card center">
+      <div v-html="myHtml"></div> <!-- v-html - can rander your html -->
+      <h2 v-text="title"></h2>
+      <h2 v-once>{{ title }}</h2> <!-- v-once - work only once, you can't change that title -->
+      <h2 v-pre>{{ title }}</h2> <!-- v-pre - for debagging -->
+      <button class="btn" @click="this.title = 'Changed title'">Change titile</button>
+    </div>
+  </div>
+</template>
